0a963c1cefd1d57e22c57d9a071e7fc64ff0a82e
Bogdan Odintsov <bacha1122001@gmail.com>
2022-09-08 23:30:01 +0300

    Добавлена авторизация и страница определенного поста

---
+++ src/redux/slices/auth.js
@@ -0,0 +1,44 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "../../axios/axios";
+
+export const fetchAuth = createAsyncThunk(
+  "auth/fetchUserData",
+  async (params) => {
+    const { data } = await axios.post("/auth/login", params);
+    return data;
+  }
+);
+
+const initialState = {
+  data: null,
+  status: "loading",
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    logout: (state) => {
+      state.data = null;
+    },
+  },
+  extraReducers: {
+    [fetchAuth.pending]: (state) => {
+      state.data = null;
+      state.status = "loading";
+    },
+    [fetchAuth.fulfilled]: (state, action) => {
+      state.data = action.payload;
+      state.status = "loaded";
+    },
+    [fetchAuth.rejected]: (state) => {
+      state.data = null;
+      state.status = "error";
+    },
+  },
+});
+
+export const selectIsAuth = (state) => Boolean(state.auth.data);
+
+export const authReducer = authSlice.reducer;
+export const { logout } = authSlice.actions;
