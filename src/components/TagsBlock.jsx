45cf9854eb4d897246f03cf0a05f11a71cd44216
Bogdan Odintsov <bacha1122001@gmail.com>
2022-09-08 16:49:46 +0300

    new init update

--- src/components/TagsBlock.jsx
+++ src/components/TagsBlock.jsx
@@ -13,27 +13,27 @@ import { SideBlock } from "./SideBlock";
 export const TagsBlock = ({ items, isLoading = true }) => {
   return (
     <SideBlock title="Тэги">
-      {/*<List>*/}
-      {/*  {(isLoading ? [...Array(5)] : items).map((name, i) => (*/}
-      {/*    <a*/}
-      {/*      style={{ textDecoration: "none", color: "black" }}*/}
-      {/*      href={`/tags/${name}`}*/}
-      {/*    >*/}
-      {/*      <ListItem key={i} disablePadding>*/}
-      {/*        <ListItemButton>*/}
-      {/*          <ListItemIcon>*/}
-      {/*            <TagIcon />*/}
-      {/*          </ListItemIcon>*/}
-      {/*          {isLoading ? (*/}
-      {/*            <Skeleton width={100} />*/}
-      {/*          ) : (*/}
-      {/*            <ListItemText primary={name} />*/}
-      {/*          )}*/}
-      {/*        </ListItemButton>*/}
-      {/*      </ListItem>*/}
-      {/*    </a>*/}
-      {/*  ))}*/}
-      {/*</List>*/}
+      <List>
+        {(isLoading ? [...Array(5)] : items).map((name, i) => (
+          <a
+            style={{ textDecoration: "none", color: "black" }}
+            href={`/tags/${name}`}
+          >
+            <ListItem key={i} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <TagIcon />
+                </ListItemIcon>
+                {isLoading ? (
+                  <Skeleton width={100} />
+                ) : (
+                  <ListItemText primary={name} />
+                )}
+              </ListItemButton>
+            </ListItem>
+          </a>
+        ))}
+      </List>
     </SideBlock>
   );
 };
